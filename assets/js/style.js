fd0672d4b62e8f3f6c98b56475c36ba60aed20a9
T1mp4ve1 <pavelwire@outlook.it>
2025-07-28 09:10:23 +0200

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,18 @@
+'use strict'
+// commento nel JS
+
+console.log('Zdarova Pazani, 567, Bongiorno');
+
+let message = 'Zdarova!';
+console.log(message);
+
+const message2 = 'Zdarova2!';
+console.log(message2);
+
+const name = 'Vasily';
+const sex = 'M';
+const age = '24';
+console.log(name, age, sex);
+
+let you = 'name'
+console.log(name)
